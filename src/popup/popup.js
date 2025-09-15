eefe476dc053520857d8e747de4290f2b7be061d
Mateus Torres <mmtorres230726@gmail.com>
2025-09-14 23:08:02 -0300

    popup finalizada em lastro com o figma

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -6,6 +6,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const saturationSlider = document.getElementById('saturation');
     const saturationValue = document.getElementById('saturation-value');
 
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    const resetButton = document.querySelector('.reset-btn');
+    const modeButtons = document.querySelectorAll('.mode-btn');
+    console.log('Mode buttons found:', modeButtons);
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            filterButtons.forEach(btn => {
+                btn.classList.remove('active');
+            });
+            event.target.classList.add('active');
+            gatherAndSendState();
+        });
+    });
+
+    resetButton.addEventListener('click', () => {
+        filterButtons.forEach(btn => btn.classList.remove('active'));
+        contrastSlider.value = 50;
+        saturationSlider.value = 50;
+        updateSliderLook(contrastSlider, contrastValue);
+        updateSliderLook(saturationSlider, saturationValue);
+        gatherAndSendState();
+});
+
+    modeButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            console.log('A mode button was clicked!');
+            event.target.classList.toggle('active');
+            gatherAndSendState();
+        });
+});
+
     function updateSliderLook(slider, valueDisplay) {
         const min = slider.min;
         const max = slider.max;
@@ -20,12 +52,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     contrastSlider.addEventListener('input', () => {
         updateSliderLook(contrastSlider, contrastValue);
+        gatherAndSendState();
     });
 
     saturationSlider.addEventListener('input', () => {
         updateSliderLook(saturationSlider, saturationValue);
+        gatherAndSendState();
     });
 
     updateSliderLook(contrastSlider, contrastValue);
     updateSliderLook(saturationSlider, saturationValue);
+
+    gatherAndSendState();
+
+    function gatherAndSendState() {
+        const activeFilter = document.querySelector('.filter-btn.active');
+
+        const settings = {
+            filter: activeFilter ? activeFilter.textContent : 'none',
+            contrast: contrastSlider.value,
+            saturation: saturationSlider.value,
+            readingMode: document.getElementById('reading-mode').classList.contains('active'),
+            nightVision: document.getElementById('night-vision').classList.contains('active')
+        }
+        console.log("state changed: ", settings);
+    };
 });
